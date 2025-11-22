435a94e785d09953bba65008c6706fdedfef5ac4
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-11-22 18:17:52 +0100

    feat(proxy) hook + module + proxy para acceder a s267 con fetch sin que CORS inhabilite la petición

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // todas las peticiones a /ogame se redirigen al servidor real
+      '/ogame': {
+        target: 'https://s267-es.ogame.gameforge.com',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/ogame/, ''),
+      },
+    },
+  },
 })
